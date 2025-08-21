c9d8ea67f481b70785c5d59754e632d82a085fd9
Joshua Raphael <josh.n.raphael@gmail.com>
2025-08-21 16:36:56 -0700

    make resource generation for tags

---
+++ scripts/generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export VERSION="$(git describe --always --dirty)"
+export VERSION_MAJOR="0"
+export VERSION_MINOR="0"
+export VERSION_PATCH="0"
+
+if [[ ${GITHUB_REF_NAME} != "" ]]; then
+    VERSION="${GITHUB_REF_NAME#v}"
+    VERSION_MAJOR=$(echo "$VERSION" | cut -d'.' -f1)
+    VERSION_MINOR=$(echo "$VERSION" | cut -d'.' -f2)
+    VERSION_PATCH=$(echo "$VERSION" | cut -d'.' -f3)
+fi
+
+# echo ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
+
+cat templates/RAScript.rc.template | envsubst > src/RAScript.rc
