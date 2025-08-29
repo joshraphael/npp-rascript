68db3debe588c0b7dbe7df61ca16eb331d6c022b
Joshua Raphael <josh.n.raphael@gmail.com>
2025-08-29 09:24:53 -0700

    bump boostregex to 1.89.0 and enable all styles

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,10 +17,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 
 - Improved styling for each mode
+- Upgraded boostregex to 1.89.0
+- Optimize local build binary performance
 
 ### Removed
-
-- Disabled header token evaluation (not performant)
 
 ## [[0.3.0](https://github.com/joshraphael/npp-rascript/releases/tag/v0.3.0)] - 2025-08-28
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -9,46 +9,47 @@ DBUG=""
 if ${ENABLE_DEBUG}; then
     DBUG=-DDEBUG
 fi;
+BUILD_OPTIONS="-std=c++17 -O3 -Wall -Werror -Wextra -DUNICODE"
 
 # TinyXML2
-${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
+${ARCH}-w64-mingw32-g++ -std=c++17 ${BUILD_OPTIONS} ${DBUG} \
 -Isrc/tinyxml2 \
 -c src/tinyxml2/tinyxml2.cpp -o out/tinyxml2.o
 
 # DefaultLexer
-${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
+${ARCH}-w64-mingw32-g++ -std=c++17 ${BUILD_OPTIONS} ${DBUG} \
 -Isrc/notepad-plus-plus/scintilla/include \
 -Isrc/notepad-plus-plus/lexilla/include \
 -c src/notepad-plus-plus/lexilla/lexlib/DefaultLexer.cxx -o out/DefaultLexer.o
 
 # LexAccessor
-${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
+${ARCH}-w64-mingw32-g++ -std=c++17 ${BUILD_OPTIONS} ${DBUG} \
 -Isrc/notepad-plus-plus/scintilla/include \
 -Isrc/notepad-plus-plus/lexilla/lexlib \
 -c src/notepad-plus-plus/lexilla/lexlib/LexAccessor.cxx -o out/LexAccessor.o
 
 # StyleContext
-${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
+${ARCH}-w64-mingw32-g++ -std=c++17 ${BUILD_OPTIONS} ${DBUG} \
 -Isrc/notepad-plus-plus/scintilla/include \
 -Isrc/notepad-plus-plus/lexilla/lexlib \
 -c src/notepad-plus-plus/lexilla/lexlib/StyleContext.cxx -o out/StyleContext.o
 
 # WordList
-${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
+${ARCH}-w64-mingw32-g++ -std=c++17 ${BUILD_OPTIONS} ${DBUG} \
 -Isrc/notepad-plus-plus/lexilla/lexlib \
 -c src/notepad-plus-plus/lexilla/lexlib/WordList.cxx -o out/WordList.o
 
 # Parser (Requires BOOST_REGEX_STANDALONE definition to compile boost regex to improve performance)
-${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
+${ARCH}-w64-mingw32-g++ -std=c++17 ${BUILD_OPTIONS} ${DBUG} \
 -DBOOST_REGEX_STANDALONE \
 -Isrc/tinyxml2 \
--Isrc/notepad-plus-plus/boostregex \
+-Isrc/regex/include \
 -Isrc/notepad-plus-plus/scintilla/include \
 -Isrc/notepad-plus-plus/PowerEditor/src/MISC/PluginsManager \
 -c src/Parser.cpp -o out/Parser.o
 
 # LexRAScript
-${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
+${ARCH}-w64-mingw32-g++ -std=c++17 ${BUILD_OPTIONS} ${DBUG} \
 -Isrc/tinyxml2 \
 -Isrc/notepad-plus-plus/scintilla/include \
 -Isrc/notepad-plus-plus/lexilla/lexlib \
@@ -60,7 +61,7 @@ ${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
 ${ARCH}-w64-mingw32-windres src/RAScript.rc -o out/rc.o
 
 # RAScript
-${ARCH}-w64-mingw32-g++ -std=c++17 -Wall -Werror -Wextra -DUNICODE ${DBUG} \
+${ARCH}-w64-mingw32-g++ -std=c++17 ${BUILD_OPTIONS} ${DBUG} \
 -Isrc/notepad-plus-plus/PowerEditor/src \
 -Isrc/notepad-plus-plus/PowerEditor/src/MISC/PluginsManager \
 -Isrc/notepad-plus-plus/scintilla/include \
